4683eb4df82fcb0860b1b29c31755bca2658691c
ThomasBezanson12 <818384@mystma.org>
2022-09-13 09:40:58 -0500

    3 functions

---
+++ main.py
@@ -0,0 +1,17 @@
+def creatorNamePrint():
+   print("Thomas Bezanson")
+def topFiveFilms():
+    print("5. One Flew Over The Cuckoo's Nest")
+    print("4. The Wolf of Wall Street")
+    print("3.Goodfellas")
+    print("2. The Shining")
+    print("1.The Prestige")
+def bestShowTV()
+    print("The answer you're looking for is Game of Thrones")
+creatorNamePrint()
+topFiveFilms()
+bestShowTV()
+
+
+
+
